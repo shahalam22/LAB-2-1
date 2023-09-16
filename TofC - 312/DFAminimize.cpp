8ff0e831bd9d4cdad485602ca906e938d3759a53
Shah Alam Abir <shahalamabir21@gmail.com>
2023-09-16 10:01:52 +0600

    dfaminimization update & complete from home

--- TofC - 312/DFAminimize.cpp
+++ TofC - 312/DFAminimize.cpp
@@ -4,8 +4,18 @@ using namespace std;
 int numOfStates, nOfEndingState, numOfSymbol;
 char ss, es[10];
 char states[10];
-// char matchedStates[10];
 
+
+int get_index(char node){
+    int i=0;
+    for(auto n: states){
+        if(n == node){
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
 
 bool isEndState(char a){
     for(int i=0; i<nOfEndingState; i++){
@@ -39,10 +49,9 @@ int main(){
     //     cin >> es[i];
     // }
 
-
-
+    // taking input from file
     fstream file;
-    file.open("practice.txt");
+    file.open("DFAminimize.txt");
 
 
     file >> numOfStates;
@@ -56,12 +65,12 @@ int main(){
         file >> es[i];
     }
 
-
+    // defining transition matrix
     char transitionMatrix[numOfStates][numOfSymbol];
     char table[numOfStates][numOfStates];
     char modifiedTable[numOfStates][6];
 
-    
+    // taking input from file for transition matrix
     for(int i=0; i<numOfStates; i++){
         for(int j=0; j<numOfSymbol; j++){
             file >> transitionMatrix[i][j];
@@ -69,6 +78,7 @@ int main(){
     }
     file.close();
 
+    // modified transition matrix for table filling for strings "0", "1", "00", "01", "10", "11
     for(int i=0; i<numOfStates; i++){
         modifiedTable[i][0] = transitionMatrix[i][0];
         modifiedTable[i][1] = transitionMatrix[i][1];
@@ -78,14 +88,12 @@ int main(){
         modifiedTable[i][5] = transitionMatrix[transitionMatrix[i][1]-'a'][1];
     }
 
-
     // for(int i=0; i<numOfStates; i++){
     //     for(int j=0; j<6; j++){
     //         cout << modifiedTable[i][j] << " ";
     //     }
     //     cout << endl;
     // }
-
 
     //table filling
     for(int i=0; i<numOfStates; i++){
@@ -99,6 +107,7 @@ int main(){
     }
 
     //print table
+    cout << "\nEquivalence table: " << endl;
     for(int i=0; i<numOfStates; i++){
         cout << states[i] << ": ";
         for(int j=0; j<i; j++){
@@ -112,4 +121,39 @@ int main(){
     }
     cout << endl;
 
+
+    //modified DFA || minimized DFA
+    char modifiedTransitionMatrix[numOfStates][numOfSymbol];
+    set<char> modifiedStates;
+
+    for(int i=0; i<numOfStates; i++){
+        modifiedStates.insert(states[i]);
+        for(int j=0; j<numOfSymbol; j++){
+            modifiedTransitionMatrix[i][j] = transitionMatrix[i][j];
+        }
+    }
+
+    cout << "\nEquibalent states: " << endl;
+
+    for(int i=0; i<numOfStates; i++){
+        for(int j=0; j<i; j++){
+            if(table[i][j] == '='){
+                cout << states[i] << "=" << states[j] << endl;
+                modifiedStates.erase(states[i]);
+                for(int k=0; k<numOfStates; k++){
+                    if(transitionMatrix[k][0] == states[i]){
+                        modifiedTransitionMatrix[k][0] = states[j];
+                    }
+                    if(transitionMatrix[k][1] == states[i]){
+                        modifiedTransitionMatrix[k][1] = states[j];
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "\nMinimized DFA: " << endl;
+    for(auto n: modifiedStates){
+        cout << n << ": " << modifiedTransitionMatrix[get_index(n)][0] << " " << modifiedTransitionMatrix[get_index(n)][1] << endl;
+    }
 }
